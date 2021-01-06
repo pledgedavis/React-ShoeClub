408d88a360be07da82c56dc523a8dd18c5f18439
pledgedavis <pledgedavis14@gmail.com>
2021-01-05 23:23:01 -0500

    adding model relationships for user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+    #loads the signup page
+ def new 
+    @user = User.new 
+ end
+
+ def home
+   #finds user by id
+   @user = User.find_by_id(session[:user_id])
+   # redirect_to user_path(@user.id)
+  end
+
+ def create 
+    #creates a new user if the user saves the session user id will be set to equal the users id and then redirect to the shoe page
+   #  binding.pry
+    @user = User.new(user_params)
+    if @user.save 
+        session[:user_id] = @user.id 
+        redirect_to home_path
+    else 
+      # if user doesn't save renders new page
+        render :new
+    end
+ end
+
+
+private 
+
+ def user_params
+    params.require(:user).permit(:username, :email, :password)
+ end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :shoes, :dependent => :delete_all
+    # deletes every shoe assigned to the user upon removing the user
+    has_many :brands, through: :shoes 
+    # validates :username, :email, presence: true 
+    # validates :username, :email, uniqueness: true 
+
 end
