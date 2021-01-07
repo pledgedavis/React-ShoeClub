f89fdb528fbb5159428f78ca35bb9da25064b651
pledgedavis <pledgedavis14@gmail.com>
2021-01-07 01:33:48 -0500

    added containers, reducers, and actions folders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+// gives react application access to the redux store 
+import { createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(
   <React.StrictMode>
