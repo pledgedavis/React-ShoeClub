0d19c77e6db05de98e3b740caf2c7f249cbe67d5
pledgedavis <pledgedavis14@gmail.com>
2021-01-07 13:42:47 -0500

    added find brand method and shoes strong  params

--- backend/app/controllers/shoes_controller.rb
+++ backend/app/controllers/shoes_controller.rb
@@ -1,6 +1,7 @@
 class ShoesController < ApplicationController
 
     def index 
+
     end
 
     def show 
@@ -12,4 +13,13 @@ class ShoesController < ApplicationController
     def delete_all
     end
 
+    private 
+
+    def find_brand
+        @brand= Brand.find_by(id: params[:brand_id])
+    end
+      
+    def shoe_params 
+        params.require(:shoe).permit(:name, :size, :price, :image, :user_id, :brand_id, brand_attributes:[ :id, :company_name])
+    end
 end
