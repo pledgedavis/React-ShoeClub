6d3997c37792af4af81c0cbf4d5ebddccc1e863e
pledgedavis <pledgedavis14@gmail.com>
2021-01-07 01:25:04 -0500

    fixed frontend bug npm working now

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -16,10 +16,7 @@ class UsersController < ApplicationController
    # POST /users
    def create
      @user = User.new(user_params)
-     @location =  Location.find_or_create_by(city: params[:user][:hometown][:city], state: params[:user][:hometown][:state], country: params[:user][:hometown][:country])
- 
-     @user.hometown = @location
-     if @user.save
+       if @user.save
        session[:user_id] = @user.id
        render json: UserSerializer.new(@user), status: :created
      else
