3826999f8f1716c3a714d165a232003a7ce8a364
pledgedavis <pledgedavis14@gmail.com>
2021-01-08 16:36:35 -0500

    creating cart page

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,5 +1,9 @@
 import React, { Fragment } from 'react';
 import { connect } from "react-redux";
+import yeezy from '../images/yeezy.jpg';
+import yeezy2 from '../images/yeezy2.jpg';
+import yeezy3 from '../images/yeezy3.jpg';
+import yeezy4 from '../images/yeezy4.jpg';
 
 const Cart = ({cartProps}) =>{
     console.log(cartProps)
@@ -13,6 +17,8 @@ const Cart = ({cartProps}) =>{
        console.log(productsInCart)
    })
 
+   const productImages = [yeezy, yeezy2, yeezy3]
+
    productsInCart = productsInCart.map( (product, index) => {
         return(
             <Fragment>
@@ -20,18 +26,15 @@ const Cart = ({cartProps}) =>{
                         <span className="sm-hide">{product.name}</span>
                         </div>
                         <div className="price sm-hide">${product.price},00</div>
-                        <div className="">
+                        <div className="quantity">
+                            <ion-icon className="increase" name="arrow-foward-circle-outline"></ion-icon>
                         </div>
+                        <div className="total">${product.numbers * product.price},00</div>
             </Fragment>
         )
 
    })
 
-    return (
-       <div>
-           <h1>Cart</h1>
-       </div>
-    )
 };
 
 const mapStateToProps = state => ({
