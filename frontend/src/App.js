08c8b8669c46e8271bb8dc8bcce55e66855871c0
pledgedavis <pledgedavis14@gmail.com>
2021-01-08 14:35:30 -0500

    Adding routes for user navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home.js'
+import Cart from "./components/Cart.js"
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+// allows me to create multiple pages ^
 
 
 function App() {
   return (
     <Provider store={store}>
      <div className="App">
+       <BrowserRouter>
+       <Route path="/" component={Home}/>
+       <Route path="/cart" component={cart}/>
+       
       <Navbar/>
       <Home/>
+      </BrowserRouter>
      </div>
     </Provider>
   );
