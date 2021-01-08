057b33f2a3595949f1177d463bdbe5e88dc710f4
pledgedavis <pledgedavis14@gmail.com>
2021-01-08 16:04:47 -0500

    creating cart page

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-const Cart = () =>{
+const Cart = ({cartProps}) =>{
+    console.log(cartProps)
+
+
+
+    
     return (
        <div>
            <h1>Cart</h1>
        </div>
     )
-}
+};
 
-export default Cart 
+const mapStateToProps = state => ({
+    cartProps: state.cartState
+});
+
+export default connect(mapStateToProps)(Cart);
