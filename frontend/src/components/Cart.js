f4908424723815ba88d6303403af47cf68c9ca32
pledgedavis <pledgedavis14@gmail.com>
2021-01-08 16:24:58 -0500

    creating cart page

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from "react-redux";
 
 const Cart = ({cartProps}) =>{
     console.log(cartProps)
 
+   let productsInCart = []
 
+   Object.keys(cartProps.products).forEach( function(item){
+       if(cartProps.products[item].incart);{
+       productsInCart.push(cartProps.products[item])
+       };
+       console.log(productsInCart)
+   })
 
-    
+   productsInCart = productsInCart.map( (product, index) => {
+        return(
+            <Fragment>
+                        <div className="product"><ion-icon name="close-circle"></ion-icon><img src={productImages[index]}/>
+                        <span className="sm-hide">{product.name}</span>
+                        </div>
+                        <div className="price sm-hide">${product.price},00</div>
+                        <div className="">
+                        </div>
+            </Fragment>
+        )
+
+   })
+
     return (
        <div>
            <h1>Cart</h1>
