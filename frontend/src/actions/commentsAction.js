283504ae6325240ccb8cb56a8d2a180fdb769e5e
pledgedavis <pledgedavis14@gmail.com>
2021-01-11 00:21:01 -0500

    updated comments action creator function

--- frontend/src/actions/commentsAction.js
+++ frontend/src/actions/commentsAction.js
@@ -1,1 +1,11 @@
-export const fetchAll
+export const fetchAllComments(){
+    return dispatch => { 
+        dispatch({type: "LOADING_COMMENTS"})
+    fetch('/shoes')
+    .then(resp => resp.json())
+    .then(shoes => dispatch({
+        type: 'FETCHED_ALL_COMMENTS',
+        payload: shoes
+    }))
+    };
+}
