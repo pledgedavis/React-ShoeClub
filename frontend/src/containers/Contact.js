9b0c34c6a38906803373b3222beadb72cb5226d4
pledgedavis <pledgedavis14@gmail.com>
2021-01-11 10:53:29 -0500

    updated contact container

--- frontend/src/containers/Contact.js
+++ frontend/src/containers/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import ContactMe from '../components/ContactMe'
+import AboutMe from '../components/AboutMe'
+
 
 
 export default function Contact() {
     return (
         <div>
-            
+              
+            <ContactMe/>
+            <AboutMe/>
         </div>
     )
 }
